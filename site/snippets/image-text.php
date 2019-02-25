169e285bd147fec2500274dfcf672a663158815f
Daniel Szecket <szecket@magrittescow.com>
2019-02-24 19:33:58 -0500

    uncroppage

--- site/snippets/image-text.php
+++ site/snippets/image-text.php
@@ -1,12 +1,10 @@
-  <div class='grid-container section'>
-    <div class='grid-x grid-padding-x'>
-<?php if($image = $data->images()->sortBy('sort', 'asc')->first()): $thumb = $image->crop(600, 600); ?>
+  <div class='grid-x grid-padding-x'>
+<?php if($image = $data->images()->sortBy('sort', 'asc')->first()): $first = $image; ?>
     <div class='medium-16 small-centered large-8 cell'>
-      <img src="<?= $thumb->url() ?>" alt="Thumbnail for <?= $data->title()->html() ?>" class="full-width" />
+      <img src="<?= $first->url() ?>" alt="Thumbnail for <?= $data->title()->html() ?>" class="full-width" />
     </div>
     <?php endif ?>
     <div class='medium-16 small-centered large-8 cell'>
       <p> <?= $data->text()->kirbytext() ?></p>
     </div>
-  </div>
   </div>
